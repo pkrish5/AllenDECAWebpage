e2cd7c09740d2017d0dfa1efb65335ac3bd912f4
PranavKrishnan05 <Pranav.Krishnan05@gmail.com>
2023-06-29 14:43:19 -0500

    trying to fix firebase compilation error

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -21,7 +21,7 @@ const Calendar = () => {
           style={{ paddingBottom: "24px" }}
         >
 
-        <iframe src="https://calendar.google.com/calendar/embed?src=a31ac6653debdbdfaa76574d38360f64df73072fcd3dc019cb8c8f7512ece366%40group.calendar.google.com&ctz=America%2FChicago"  width="800" height="600"  scrolling="no"></iframe>
+        {/*<iframe src="https://calendar.google.com/calendar/embed?src=a31ac6653debdbdfaa76574d38360f64df73072fcd3dc019cb8c8f7512ece366%40group.calendar.google.com&ctz=America%2FChicago"  width="800" height="600"  scrolling="no"></iframe>*/}
 </div>
     </>
     
