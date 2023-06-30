70565d7e53a2b7f55e0d1b59199bc72fabb72dfa
PranavKrishnan05 <Pranav.Krishnan05@gmail.com>
2023-06-30 03:20:08 -0500

    updated calandar hopefully

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -5,7 +5,7 @@ const Calendar = () => {
   return (
     
     <>
-      {/*<script data-cfasync="false" data-tockify-script="embed" src="https://public.tockify.com/browser/embed.js"></script>*/}
+      <script data-cfasync="false" data-tockify-script="embed" src="https://public.tockify.com/browser/embed.js"></script>
       <Breadcrumb
       
         pageName="Calendar"
@@ -15,13 +15,13 @@ const Calendar = () => {
         <h2 className="text-3xl font-bold mb-8 text-center text-black dark:text-white">
           Keep up with all of our events!
         </h2>
-        {/*<div data-tockify-component="calendar" data-tockify-calendar="pranav.krishnan" ></div>*/}
+        <div data-tockify-component="calendar" data-tockify-calendar="pranav.krishnan" ></div>
         <div
           className="flex justify-center items-center"
           style={{ paddingBottom: "24px" }}
         >
 
-        {/*<iframe src="https://calendar.google.com/calendar/embed?src=a31ac6653debdbdfaa76574d38360f64df73072fcd3dc019cb8c8f7512ece366%40group.calendar.google.com&ctz=America%2FChicago"  width="800" height="600"  scrolling="no"></iframe>*/}
+       
 </div>
     </>
     
