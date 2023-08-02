3ed57766a1f06686f3c972b1d3501b7f36de619c
Pranav krishnan <88994955+pkrish5@users.noreply.github.com>
2023-08-02 13:18:57 -0500

    Update index.tsx

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -15,13 +15,12 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                The Official Website for
                 </h1>
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                   Allen  DECA
+                   Allen DECA
                    </h1>
                    <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
-                  This website has everything you need to know about DECA at Allen Highschool 
+                  Everything you need to know about DECA at Allen Highschool 
                   </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
