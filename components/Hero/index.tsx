91ecc5c21b6ddcb12ecd1294dad91b2e31f0d5b0
PranavKrishnan05 <Pranav.Krishnan05@gmail.com>
2023-08-02 16:13:24 -0500

    adjusted margin %

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
         id="home"
         className="relative z-10 overflow-hidden pt-[120px] pb-16 md:pt-[150px] md:pb-[120px] xl:pt-[180px] xl:pb-[160px] 2xl:pt-[210px] 2xl:pb-[200px]"
       >
-        <div className="container">
+        <div className="container" style={{marginBottom: "20%"}} >
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4">
               <div
@@ -286,12 +286,12 @@ const Hero = () => {
             </defs>
           </svg>
         </div>
+        {/* <br></br>
         <br></br>
         <br></br>
         <br></br>
         <br></br>
-        <br></br>
-        <br></br>
+        <br></br> */}
       </section>
 
     </>
